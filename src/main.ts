ce3489732e686db2cb4a7af27f692f138abe712f
Tulio <tuliojr3101@gmail.com>
2026-04-12 20:24:19 -0500

    feat: adding logs to debug on azure

--- src/main.ts
+++ src/main.ts
@@ -21,7 +21,11 @@ async function bootstrap() {
     }),
   );
 
-  await app.listen(process.env.port || 3000, "0.0.0.0");
+  const port = process.env.PORT || 3000;
+
+  await app.listen(port, "0.0.0.0");
+
+  console.log(`El servidor escucha en: ${port}`);
 
 }
 bootstrap();
